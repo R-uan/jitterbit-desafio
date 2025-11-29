4255d058a828fbeb18892201ca228486902f9c6f
Ruan Lopes <rpo.lopes@hotmail.com>
2025-11-29 16:48:51 -0300

    fix: compatibility issues
    NixOS does not have the latest prisma-engines package so I had to downgrade it

--- flake.nix
+++ flake.nix
@@ -21,14 +21,14 @@
             pkgs.nodePackages.typescript-language-server
           ];
 
-          shellHook = ''
-            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";
-            export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
-            export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
-            export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
-            export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
-            echo "Node $(node -v) ready. Try not to break anything."
-          '';
+          env = {
+            PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine";
+            PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine";
+            PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node";
+            PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt";
+          };
+
+          shellHook = ''echo "Node $(node -v) ready. Try not to break anything."'';
         };
       });
 }
